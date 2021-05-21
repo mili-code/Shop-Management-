48217d410e32d4f84e085de507f30ce68747aa57
tazin2021 <tazinafrin17@gmail.com>
2021-05-21 23:03:29 +0600

    done

--- Shop Management/src/shop/management/grocerries.java
+++ Shop Management/src/shop/management/grocerries.java
@@ -5,6 +5,11 @@
  */
 package shop.management;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author user
@@ -47,12 +52,12 @@ public class grocerries extends javax.swing.JFrame {
         sabuy = new javax.swing.JButton();
         bbuy = new javax.swing.JButton();
         obuy = new javax.swing.JButton();
-        squantity = new javax.swing.JSpinner();
-        rquantity = new javax.swing.JSpinner();
-        oquantity = new javax.swing.JSpinner();
-        bquantity = new javax.swing.JSpinner();
-        saquantity = new javax.swing.JSpinner();
         jButton21 = new javax.swing.JButton();
+        squantity = new javax.swing.JTextField();
+        oquantity = new javax.swing.JTextField();
+        saquantity = new javax.swing.JTextField();
+        bquantity = new javax.swing.JTextField();
+        rquantity = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -150,20 +155,22 @@ public class grocerries extends javax.swing.JFrame {
             }
         });
 
-        squantity.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-
-        rquantity.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-
-        oquantity.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-
-        bquantity.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-
-        saquantity.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-
         jButton21.setText("Baxk");
         jButton21.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton21ActionPerformed(evt);
+            }
+        });
+
+        squantity.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                squantityActionPerformed(evt);
+            }
+        });
+
+        saquantity.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                saquantityActionPerformed(evt);
             }
         });
 
@@ -189,16 +196,27 @@ public class grocerries extends javax.swing.JFrame {
                     .addGroup(jPanel4Layout.createSequentialGroup()
                         .addGap(3, 3, 3)
                         .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                                .addComponent(rprice)
-                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(jPanel4Layout.createSequentialGroup()
+                                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(bprice, javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(sapricee, javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(rprice, javax.swing.GroupLayout.Alignment.LEADING))
+                                .addGap(140, 140, 140)
+                                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(saquantity, javax.swing.GroupLayout.DEFAULT_SIZE, 55, Short.MAX_VALUE)
+                                        .addComponent(bquantity))
+                                    .addComponent(rquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 143, Short.MAX_VALUE))
                             .addGroup(jPanel4Layout.createSequentialGroup()
                                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(oprice)
                                     .addComponent(sprice)
-                                    .addComponent(bprice)
-                                    .addComponent(sapricee))
-                                .addGap(245, 338, Short.MAX_VALUE)))
+                                    .addComponent(oprice))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(oquantity, javax.swing.GroupLayout.DEFAULT_SIZE, 55, Short.MAX_VALUE)
+                                    .addComponent(squantity))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                         .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(rbuy)
                             .addComponent(sbuy)
@@ -212,13 +230,7 @@ public class grocerries extends javax.swing.JFrame {
                             .addGroup(jPanel4Layout.createSequentialGroup()
                                 .addComponent(jLabel43)
                                 .addGap(133, 133, 133)
-                                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(rquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel56)
-                                    .addComponent(squantity, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(oquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(saquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(bquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addComponent(jLabel56)))
                         .addGap(0, 0, Short.MAX_VALUE))))
         );
         jPanel4Layout.setVerticalGroup(
@@ -236,32 +248,42 @@ public class grocerries extends javax.swing.JFrame {
                     .addComponent(rprice)
                     .addComponent(jLabel49)
                     .addComponent(rbuy, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(rquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(41, 41, 41)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel46)
-                    .addComponent(sprice)
-                    .addComponent(sbuy, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(squantity, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(41, 41, 41)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(oprice)
-                    .addComponent(jLabel47)
-                    .addComponent(obuy, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(oquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addGap(9, 9, 9)
+                        .addComponent(rquantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addGap(41, 41, 41)
+                        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel46)
+                            .addComponent(sprice)
+                            .addComponent(sbuy, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(squantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(41, 41, 41)
+                        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(oprice)
+                            .addComponent(jLabel47)
+                            .addComponent(obuy, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addGap(135, 135, 135)
+                        .addComponent(oquantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(39, 39, 39)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel48)
                     .addComponent(sapricee)
                     .addComponent(sabuy, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(saquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addComponent(saquantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(23, 23, 23)))
                 .addGap(33, 33, 33)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(bprice)
                     .addComponent(jLabel50)
                     .addComponent(bbuy, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(bquantity, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addGap(9, 9, 9)
+                        .addComponent(bquantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
                 .addComponent(jButton21, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(20, 20, 20))
         );
@@ -306,9 +328,52 @@ public class grocerries extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        Payment py = new Payment();
-       py.setVisible(true);
-       this.dispose(); 
+  
+        String rp = rprice.getText();
+        String rq = rquantity.getText();
+        if( rp.isEmpty() || rq.isEmpty()){
+             JOptionPane.showMessageDialog(this, "Fill up the form properly.", 
+                     "Error", JOptionPane.ERROR_MESSAGE);
+        }else{
+
+             Connection dbcon = DBconnect.connectDB();
+
+            try {
+                PreparedStatement st = (PreparedStatement)
+                dbcon.prepareStatement("INSERT INTO rice (rprice,rquantity) VALUES(?,?)");
+
+
+
+            st.setString(1, rp);
+            st.setString(2, rq);
+          
+
+
+
+
+
+            int rs = st.executeUpdate();
+
+
+
+            JOptionPane.showMessageDialog(this, "Buy Successful.", 
+                    "Success", JOptionPane.INFORMATION_MESSAGE);
+
+               dispose();
+               Payment hm = new Payment();
+               hm.setVisible(true);
+
+            } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(this, "Fill up the form properly.", 
+                     "Error", JOptionPane.ERROR_MESSAGE);
+
+            rprice.setText("");
+            rprice.requestFocus();
+            rquantity.setText("");
+
+        }
+
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton21ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton21ActionPerformed
@@ -316,6 +381,14 @@ public class grocerries extends javax.swing.JFrame {
 hm.setVisible(true);
 this.dispose();
     }//GEN-LAST:event_jButton21ActionPerformed
+
+    private void squantityActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_squantityActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_squantityActionPerformed
+
+    private void saquantityActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saquantityActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_saquantityActionPerformed
 
     /**
      * @param args the command line arguments
@@ -355,7 +428,7 @@ this.dispose();
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bbuy;
     private javax.swing.JLabel bprice;
-    private javax.swing.JSpinner bquantity;
+    private javax.swing.JTextField bquantity;
     private javax.swing.JButton jButton21;
     private javax.swing.JLabel jLabel43;
     private javax.swing.JLabel jLabel44;
@@ -369,15 +442,15 @@ this.dispose();
     private javax.swing.JPanel jPanel4;
     private javax.swing.JButton obuy;
     private javax.swing.JLabel oprice;
-    private javax.swing.JSpinner oquantity;
+    private javax.swing.JTextField oquantity;
     private javax.swing.JButton rbuy;
     private javax.swing.JLabel rprice;
-    private javax.swing.JSpinner rquantity;
+    private javax.swing.JTextField rquantity;
     private javax.swing.JButton sabuy;
     private javax.swing.JLabel sapricee;
-    private javax.swing.JSpinner saquantity;
+    private javax.swing.JTextField saquantity;
     private javax.swing.JButton sbuy;
     private javax.swing.JLabel sprice;
-    private javax.swing.JSpinner squantity;
+    private javax.swing.JTextField squantity;
     // End of variables declaration//GEN-END:variables
 }
